60e0cb3301bb959aec4d82e0e851ec79fa58bcd9
Jérémie Drouet <jeremie.drouet@gmail.com>
2023-03-25 14:44:38 +0100

    chore: move search page parsing to dedicated module
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- indexer-bitsearch/src/lib.rs
+++ indexer-bitsearch/src/lib.rs
@@ -1,37 +1,20 @@
-use chrono::{DateTime, Utc};
 use manteau_indexer_prelude::{
-    Category, Indexer, IndexerBuilder, IndexerEntry, IndexerError, IndexerErrorReason,
-    IndexerResult,
+    Category, Indexer, IndexerBuilder, IndexerError, IndexerErrorReason, IndexerResult,
 };
-use once_cell::sync::Lazy;
 use reqwest::IntoUrl;
-use scraper::{ElementRef, Html, Selector};
 use url::Url;
 
 mod date;
-
-static ROW_SELECTOR: Lazy<Selector> = Lazy::new(|| Selector::parse(".card.search-result").unwrap());
-static SEARCH_ROW_NAME_SELECTOR: Lazy<Selector> =
-    Lazy::new(|| Selector::parse("h5.title a").unwrap());
-static SEARCH_ROW_SIZE_SELECTOR: Lazy<Selector> =
-    Lazy::new(|| Selector::parse(".stats div:nth-child(2)").unwrap());
-static SEARCH_ROW_SEEDER_SELECTOR: Lazy<Selector> =
-    Lazy::new(|| Selector::parse(".stats div:nth-child(3)").unwrap());
-static SEARCH_ROW_LEECHER_SELECTOR: Lazy<Selector> =
-    Lazy::new(|| Selector::parse(".stats div:nth-child(4)").unwrap());
-static SEARCH_ROW_DATE_SELECTOR: Lazy<Selector> =
-    Lazy::new(|| Selector::parse(".stats img[alt=\"Date\"]").unwrap());
-static SEARCH_ROW_MAGNET_SELECTOR: Lazy<Selector> =
-    Lazy::new(|| Selector::parse("a.dl-magnet").unwrap());
+mod search;
 
 const BASE_URL: &str = "https://bitsearch.to";
-const INDEXER_NAME: &str = "bitsearch";
+pub const NAME: &str = "bitsearch";
 
 async fn fetch_page<U: IntoUrl + ToString>(url: U) -> Result<String, IndexerError> {
     let url_str = url.to_string();
     let req = reqwest::get(url).await.map_err(|err| {
         IndexerError::new(
-            INDEXER_NAME,
+            NAME,
             IndexerErrorReason::UnableToQuery {
                 url: url_str.clone(),
                 cause: err.to_string(),
@@ -40,151 +23,13 @@ async fn fetch_page<U: IntoUrl + ToString>(url: U) -> Result<String, IndexerErro
     })?;
     req.text().await.map_err(|err| {
         IndexerError::new(
-            INDEXER_NAME,
+            NAME,
             IndexerErrorReason::UnableToRead {
                 url: url_str,
                 cause: err.to_string(),
             },
         )
     })
-}
-
-fn sanitize_name(input: &str) -> String {
-    input
-        .split(&['\n', ' ', '\t'])
-        .filter(|sec| !sec.is_empty())
-        .collect::<Vec<_>>()
-        .join(" ")
-}
-
-fn parse_link_element<'a>(elt: &'a ElementRef) -> Result<ElementRef<'a>, IndexerError> {
-    elt.select(&SEARCH_ROW_NAME_SELECTOR)
-        .next()
-        .ok_or_else(|| IndexerError::new(INDEXER_NAME, IndexerErrorReason::EntryNameNotFound))
-}
-
-fn parse_link<'a>(elt: &'a ElementRef) -> Result<(String, &'a str), IndexerError> {
-    let link = parse_link_element(elt)?;
-    let name = link.text().collect::<String>();
-    let name = sanitize_name(&name);
-    let path = link
-        .value()
-        .attr("href")
-        .ok_or_else(|| IndexerError::new(INDEXER_NAME, IndexerErrorReason::EntryLinkNotFound))?;
-    Ok((name, path))
-}
-
-fn parse_size(elt: &ElementRef) -> Result<bytesize::ByteSize, IndexerError> {
-    let value = elt
-        .select(&SEARCH_ROW_SIZE_SELECTOR)
-        .next()
-        .ok_or_else(|| IndexerError::new(INDEXER_NAME, IndexerErrorReason::EntryLinkNotFound))?;
-    value
-        .text()
-        .collect::<String>()
-        .trim()
-        .parse::<bytesize::ByteSize>()
-        .map_err(|cause| {
-            IndexerError::new(INDEXER_NAME, IndexerErrorReason::EntrySizeInvalid { cause })
-        })
-}
-
-fn parse_seeders(elt: &ElementRef) -> Result<u32, IndexerError> {
-    let value = elt
-        .select(&SEARCH_ROW_SEEDER_SELECTOR)
-        .next()
-        .ok_or_else(|| IndexerError::new(INDEXER_NAME, IndexerErrorReason::EntrySeedersNotFound))?;
-    value
-        .text()
-        .collect::<String>()
-        .trim()
-        .parse::<u32>()
-        .map_err(|cause| {
-            IndexerError::new(
-                INDEXER_NAME,
-                IndexerErrorReason::EntrySeedersInvalid { cause },
-            )
-        })
-}
-
-fn parse_leechers(elt: &ElementRef) -> Result<u32, IndexerError> {
-    let value = elt
-        .select(&SEARCH_ROW_LEECHER_SELECTOR)
-        .next()
-        .ok_or_else(|| {
-            IndexerError::new(INDEXER_NAME, IndexerErrorReason::EntryLeechersNotFound)
-        })?;
-    value
-        .text()
-        .collect::<String>()
-        .trim()
-        .parse::<u32>()
-        .map_err(|cause| {
-            IndexerError::new(
-                INDEXER_NAME,
-                IndexerErrorReason::EntryLeechersInvalid { cause },
-            )
-        })
-}
-
-fn parse_date(elt: &ElementRef) -> Result<DateTime<Utc>, IndexerError> {
-    let value = elt
-        .select(&SEARCH_ROW_DATE_SELECTOR)
-        .next()
-        .and_then(|child| child.parent())
-        .and_then(|child| child.children().find_map(|c| c.value().as_text()))
-        .map(|text| text.to_string())
-        .ok_or_else(|| IndexerError::new(INDEXER_NAME, IndexerErrorReason::EntryDateNotFound))?;
-    date::parse(&value).map_err(|cause| {
-        IndexerError::new(INDEXER_NAME, IndexerErrorReason::EntryDateInvalid { cause })
-    })
-}
-
-fn parse_magnet(elt: &ElementRef) -> Result<String, IndexerError> {
-    let value = elt
-        .select(&SEARCH_ROW_MAGNET_SELECTOR)
-        .next()
-        .ok_or_else(|| IndexerError::new(INDEXER_NAME, IndexerErrorReason::EntryMagnetNotFound))?;
-    value
-        .value()
-        .attr("href")
-        .map(String::from)
-        .ok_or_else(|| IndexerError::new(INDEXER_NAME, IndexerErrorReason::EntryMagnetNotFound))
-}
-
-fn parse_row_result(base_url: &str, elt: ElementRef) -> Result<IndexerEntry, IndexerError> {
-    let (name, path) = parse_link(&elt)?;
-    let size = parse_size(&elt)?;
-    let seeders = parse_seeders(&elt)?;
-    let leechers = parse_leechers(&elt)?;
-    let date = parse_date(&elt)?;
-    let magnet = parse_magnet(&elt)?;
-
-    Ok(IndexerEntry {
-        name,
-        url: format!("{base_url}{path}"),
-        date,
-        size,
-        seeders,
-        leechers,
-        magnet,
-        origin: INDEXER_NAME,
-    })
-}
-
-fn parse_search_result(base_url: &str, html: &str) -> IndexerResult {
-    let mut results = IndexerResult::default();
-
-    let html = Html::parse_document(html);
-
-    for element in html.select(&ROW_SELECTOR) {
-        match parse_row_result(base_url, element) {
-            Ok(found) => results.entries.push(found),
-            Err(error) => results.errors.push(error),
-        };
-    }
-
-    results
 }
 
 #[derive(Debug, serde::Deserialize)]
@@ -239,7 +84,7 @@ impl Indexer for IndexerBitsearch {
             Ok(value) => value,
             Err(cause) => {
                 return IndexerResult::from(IndexerError::new(
-                    INDEXER_NAME,
+                    NAME,
                     IndexerErrorReason::UnableToBuildUrl { cause },
                 ));
             }
@@ -250,7 +95,7 @@ impl Indexer for IndexerBitsearch {
             Err(error) => return IndexerResult::from(error),
         };
 
-        parse_search_result(&self.base_url, html.as_str())
+        search::parse(&self.base_url, html.as_str())
     }
 
     async fn feed(&self, category: Category) -> IndexerResult {
@@ -268,7 +113,7 @@ impl Indexer for IndexerBitsearch {
             Err(error) => return IndexerResult::from(error),
         };
 
-        parse_search_result(&self.base_url, html.as_str())
+        search::parse(&self.base_url, html.as_str())
     }
 }
 
---
+++ indexer-bitsearch/src/search.rs
@@ -0,0 +1,154 @@
+use chrono::{DateTime, Utc};
+use manteau_indexer_prelude::{IndexerEntry, IndexerError, IndexerErrorReason, IndexerResult};
+use once_cell::sync::Lazy;
+use scraper::{ElementRef, Html, Selector};
+
+static ROW_SELECTOR: Lazy<Selector> = Lazy::new(|| Selector::parse(".card.search-result").unwrap());
+static SEARCH_ROW_NAME_SELECTOR: Lazy<Selector> =
+    Lazy::new(|| Selector::parse("h5.title a").unwrap());
+static SEARCH_ROW_SIZE_SELECTOR: Lazy<Selector> =
+    Lazy::new(|| Selector::parse(".stats div:nth-child(2)").unwrap());
+static SEARCH_ROW_SEEDER_SELECTOR: Lazy<Selector> =
+    Lazy::new(|| Selector::parse(".stats div:nth-child(3)").unwrap());
+static SEARCH_ROW_LEECHER_SELECTOR: Lazy<Selector> =
+    Lazy::new(|| Selector::parse(".stats div:nth-child(4)").unwrap());
+static SEARCH_ROW_DATE_SELECTOR: Lazy<Selector> =
+    Lazy::new(|| Selector::parse(".stats img[alt=\"Date\"]").unwrap());
+static SEARCH_ROW_MAGNET_SELECTOR: Lazy<Selector> =
+    Lazy::new(|| Selector::parse("a.dl-magnet").unwrap());
+
+fn sanitize_name(input: &str) -> String {
+    input
+        .split(&['\n', ' ', '\t'])
+        .filter(|sec| !sec.is_empty())
+        .collect::<Vec<_>>()
+        .join(" ")
+}
+
+fn parse_link_element<'a>(elt: &'a ElementRef) -> Result<ElementRef<'a>, IndexerError> {
+    elt.select(&SEARCH_ROW_NAME_SELECTOR)
+        .next()
+        .ok_or_else(|| IndexerError::new(super::NAME, IndexerErrorReason::EntryNameNotFound))
+}
+
+fn parse_link<'a>(elt: &'a ElementRef) -> Result<(String, &'a str), IndexerError> {
+    let link = parse_link_element(elt)?;
+    let name = link.text().collect::<String>();
+    let name = sanitize_name(&name);
+    let path = link
+        .value()
+        .attr("href")
+        .ok_or_else(|| IndexerError::new(super::NAME, IndexerErrorReason::EntryLinkNotFound))?;
+    Ok((name, path))
+}
+
+fn parse_size(elt: &ElementRef) -> Result<bytesize::ByteSize, IndexerError> {
+    let value = elt
+        .select(&SEARCH_ROW_SIZE_SELECTOR)
+        .next()
+        .ok_or_else(|| IndexerError::new(super::NAME, IndexerErrorReason::EntryLinkNotFound))?;
+    value
+        .text()
+        .collect::<String>()
+        .trim()
+        .parse::<bytesize::ByteSize>()
+        .map_err(|cause| {
+            IndexerError::new(super::NAME, IndexerErrorReason::EntrySizeInvalid { cause })
+        })
+}
+
+fn parse_seeders(elt: &ElementRef) -> Result<u32, IndexerError> {
+    let value = elt
+        .select(&SEARCH_ROW_SEEDER_SELECTOR)
+        .next()
+        .ok_or_else(|| IndexerError::new(super::NAME, IndexerErrorReason::EntrySeedersNotFound))?;
+    value
+        .text()
+        .collect::<String>()
+        .trim()
+        .parse::<u32>()
+        .map_err(|cause| {
+            IndexerError::new(
+                super::NAME,
+                IndexerErrorReason::EntrySeedersInvalid { cause },
+            )
+        })
+}
+
+fn parse_leechers(elt: &ElementRef) -> Result<u32, IndexerError> {
+    let value = elt
+        .select(&SEARCH_ROW_LEECHER_SELECTOR)
+        .next()
+        .ok_or_else(|| IndexerError::new(super::NAME, IndexerErrorReason::EntryLeechersNotFound))?;
+    value
+        .text()
+        .collect::<String>()
+        .trim()
+        .parse::<u32>()
+        .map_err(|cause| {
+            IndexerError::new(
+                super::NAME,
+                IndexerErrorReason::EntryLeechersInvalid { cause },
+            )
+        })
+}
+
+fn parse_date(elt: &ElementRef) -> Result<DateTime<Utc>, IndexerError> {
+    let value = elt
+        .select(&SEARCH_ROW_DATE_SELECTOR)
+        .next()
+        .and_then(|child| child.parent())
+        .and_then(|child| child.children().find_map(|c| c.value().as_text()))
+        .map(|text| text.to_string())
+        .ok_or_else(|| IndexerError::new(super::NAME, IndexerErrorReason::EntryDateNotFound))?;
+    crate::date::parse(&value).map_err(|cause| {
+        IndexerError::new(super::NAME, IndexerErrorReason::EntryDateInvalid { cause })
+    })
+}
+
+fn parse_magnet(elt: &ElementRef) -> Result<String, IndexerError> {
+    let value = elt
+        .select(&SEARCH_ROW_MAGNET_SELECTOR)
+        .next()
+        .ok_or_else(|| IndexerError::new(super::NAME, IndexerErrorReason::EntryMagnetNotFound))?;
+    value
+        .value()
+        .attr("href")
+        .map(String::from)
+        .ok_or_else(|| IndexerError::new(super::NAME, IndexerErrorReason::EntryMagnetNotFound))
+}
+
+fn parse_row_result(base_url: &str, elt: ElementRef) -> Result<IndexerEntry, IndexerError> {
+    let (name, path) = parse_link(&elt)?;
+    let size = parse_size(&elt)?;
+    let seeders = parse_seeders(&elt)?;
+    let leechers = parse_leechers(&elt)?;
+    let date = parse_date(&elt)?;
+    let magnet = parse_magnet(&elt)?;
+
+    Ok(IndexerEntry {
+        name,
+        url: format!("{base_url}{path}"),
+        date,
+        size,
+        seeders,
+        leechers,
+        magnet,
+        origin: super::NAME,
+    })
+}
+
+pub fn parse(base_url: &str, html: &str) -> IndexerResult {
+    let mut results = IndexerResult::default();
+
+    let html = Html::parse_document(html);
+
+    for element in html.select(&ROW_SELECTOR) {
+        match parse_row_result(base_url, element) {
+            Ok(found) => results.entries.push(found),
+            Err(error) => results.errors.push(error),
+        };
+    }
+
+    results
+}
