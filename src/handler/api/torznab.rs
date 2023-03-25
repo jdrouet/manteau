74b85eb97c6bbcbc5bb39b682540026b6650f3c1
Jérémie Drouet <jeremie.drouet@gmail.com>
2023-03-25 14:44:37 +0100

    chore: debug style
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- src/handler/api/torznab.rs
+++ src/handler/api/torznab.rs
@@ -1,21 +1,27 @@
 use axum::extract::Query;
 use std::borrow::Cow;
 
-#[derive(Debug, serde::Deserialize)]
-#[serde(tag = "t", rename_all = "lowercase")]
-pub enum QueryParams {
-    Caps,
-}
+// #[derive(Debug, serde::Deserialize)]
+// #[serde(tag = "t", rename_all = "lowercase")]
+// pub enum QueryParams {
+//     Caps,
+// }
 
-impl QueryParams {
-    fn handle(&self) -> Cow<'static, str> {
-        match self {
-            Self::Caps => Cow::Borrowed(include_str!("./capabilities.xml")),
-        }
-    }
-}
+// impl QueryParams {
+//     fn handle(&self) -> Cow<'static, str> {
+//         match self {
+//             Self::Caps => Cow::Borrowed(include_str!("./capabilities.xml")),
+//         }
+//     }
+// }
 
-pub async fn handler(Query(params): Query<QueryParams>) -> Cow<'static, str> {
+pub async fn handler(
+    Query(params): Query<std::collections::HashMap<String, String>>,
+) -> Cow<'static, str> {
     tracing::debug!("GET /api/torznab params={params:?}");
-    params.handle()
+    // params.handle()
+    match params.get("t").map(|v| v.as_str()) {
+        Some("cap") => Cow::Borrowed(include_str!("./capabilities.xml")),
+        _ => Cow::Borrowed("Hello World"),
+    }
 }
